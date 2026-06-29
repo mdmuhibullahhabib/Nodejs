82e24a43f048b7408905660a818341172c637a89
Muhibullah Habib <muhibullahhabib@Muhibullahs-Mac-mini.local>
2026-06-29 22:43:58 +0600

    put

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { readProduct } from "../service/product.service";
+import { insertProduct, readProduct } from "../service/product.service";
 import { IProduct } from "../types/product.type";
 import { parseBody } from "../utlity/parse.Body";
 
@@ -51,16 +51,56 @@ export const poductController = async(
     } else if (method === "POST" && url === '/products') {
 
         const body = await parseBody(req);
-        console.log(body,"body");
-        
+        const products = readProduct();
+
+        const newProduct = {
+            id: Date.now(),
+            ...body
+        }
+        // console.log(newProduct,"new");
+        products.push(newProduct);
+
+        insertProduct(products)
+        // console.log(products);
 
         res.writeHead(200, ({ "content-type": "application/json" }))
         res.end(
             JSON.stringify({
                 message: "product create successfully...",
+                data: insertProduct,
             })
         )
+    }else if (method=== "PUT"  && id !==null) {
+        const body = await parseBody(req)
+        const products = readProduct()
 
+        const index = products.findIndex((p : IProduct) =>p.id === id);
+        console.log(index,"index")
+
+        if(index <0){
+
+            res.writeHead(404, ({ "content-type": "application/json" }))
+            res.end(
+                JSON.stringify({
+                    message: "product not found...",
+                    data: null,
+                })
+            )
+        }
+        // console.log(products[index]);
+        products[index] = {id: products[index].id, ...body}
+
+        insertProduct(products)
+
+        res.writeHead(200, ({ "content-type": "application/json" }))
+        res.end(
+            JSON.stringify({
+                message: "product updated...",
+                data: products[index],
+            })
+        )
+        
+        
     }
 
 }
